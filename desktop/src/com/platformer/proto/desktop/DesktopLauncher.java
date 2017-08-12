9edcfdb0d21b08aeb11efede9571a6a680cdf0da
MrDoggo <experimentalpatrick@gmail.com>
2017-08-12 17:27:00 +0200

    Set up project structure for LibGDX

---
+++ desktop/src/com/platformer/proto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.platformer.proto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.platformer.proto.PlatformerProto;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformerProto(), config);
+	}
+}
