5edd5f2ef3d18d3ceaab54c814924c97c80a9c8a
MrDoggo <experimentalpatrick@gmail.com>
2017-08-13 13:38:56 +0200

    Add orthographic camera

--- core/src/com/platformer/proto/PlatformerProto.java
+++ core/src/com/platformer/proto/PlatformerProto.java
@@ -2,24 +2,51 @@ package com.platformer.proto;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PlatformerProto extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	private OrthographicCamera cam;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		// The camera will represent the in-game position
+		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+		cam.update();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		// Clear screen with black color
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			cam.translate(-1, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			cam.translate(1, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			cam.translate(0, -1, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			cam.translate(0, 1, 0);
+		}
+
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
--- desktop/src/com/platformer/proto/desktop/DesktopLauncher.java
+++ desktop/src/com/platformer/proto/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.platformer.proto.PlatformerProto;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Platformer Prototype";
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new PlatformerProto(), config);
 	}
 }
